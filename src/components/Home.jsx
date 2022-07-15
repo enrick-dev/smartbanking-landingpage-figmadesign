5900708ef65564914251ac4979635595490abebb
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-07-14 21:30:59 -0300

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,20 @@ function Home() {
   return (
     <div className='w-full h-screen flex '>
       {/* Container */}
-      <div className=' px-5 pt-[52px] w-full mx-auto flex flex-col items-center bg-slate-500'>
+      <div className=' px-5 pt-[52px] w-full mx-auto flex flex-col items-center'>
         
         <Navbar/>
 
-        <div  className='bg-[#F8FCFF] w-screen max-w-[1300px]  items-center justify-between  '>
+        <div  className=' grid grid-cols-12 gap-4  bg-[#F8FCFF] w-screen max-w-[1300px]  items-center justify-between  '>
           
-          <div className='flex box'>
-            <h1>banking more smart</h1>
-            <p>meet the only spend management platform and corporate card</p>
-            <button></button><p>get your card</p>
+          <div className=' col-span-5'>
+            <h1 id='titlebank' className='text-[80px] font-bold'>Banking </h1>
+            <h1 id='titlebank' className='text-[80px] font-bold'>more smart</h1>
+            <p>Meet the only spend management platform and corporate card.</p>
+            <button></button><p>Get your card</p>
           </div>
-          <div>
-              <div> </div>
+          <div className='col-span-7  bg-[#80D1FF] w-full h-full'>
+              <div className=''></div>
           </div>
 
         </div>
